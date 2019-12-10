47ee236228e7a6ada04a0f4ef341a00f5fa808c6
maxpwilson <48170841+maxpwilson@users.noreply.github.com>
2019-12-10 14:20:20 -0600

    deploy test

--- docs/make.jl
+++ docs/make.jl
@@ -12,7 +12,7 @@ deploydocs(
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() inthe Documenter manual
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 #=deploydocs(
     repo = "<repository url>"
