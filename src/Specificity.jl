c34b48746f8d3245b7b4798c0a9e7f8d65b9b181
maxpwilson <48170841+maxpwilson@users.noreply.github.com>
2019-12-10 14:35:49 -0600

    Updated documentation

--- src/Specificity.jl
+++ src/Specificity.jl
@@ -9,7 +9,7 @@ TRANSCRIPTGENE = Dict{String, String}()
 """
     download_RefSeq(::UnitRange{Int64}=1:8, ::String=PATH)
 
-Downloads mRNA reference sequence from ftp://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/mRNA_Prot/ to the PATH folder 
+Downloads mRNA reference sequence from ftp://ftp.ncbi.nlm.nih.gov/refseq/H\_sapiens/mRNA\_Prot/ to the PATH folder.  Defaults to downloading 8 files.
 """
 function download_RefSeq(num::UnitRange{Int64} = 1:8, path::String=PATH)
     p = Progress(num[end], 0.1, "Updating Reference Sequence ... ", 50)
@@ -92,6 +92,11 @@ function save_RefSeq(path::String=PATH)
     @save "$(path)Human_mRNA_allT.bson" allT
 end
 
+"""
+    load_RefSeq(::String=PATH)
+
+Loads necessary datastructures into memory
+"""
 function load_RefSeq(path::String=PATH)
     if length(ALLT) == 0 
         @load "$path\\Human_mRNA_allT.bson" allT
