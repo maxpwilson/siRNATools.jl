fad32ca013e949c77f40e569c10c5af15733234f
maxpwilson <48170841+maxpwilson@users.noreply.github.com>
2020-01-03 07:57:27 -0600

    added Genome Search Documentation (#40)

--- src/Specificity.jl
+++ src/Specificity.jl
@@ -4,10 +4,10 @@ using BSON: @save, @load
 
 """
 Structure for reference sequences.  Compresses RNA data into 2 bits of information from the 8 of a normal character string.  Can only use bases A, C, G, U.
-A => 00
-C => 01
-G => 10
-U => 11
+- A => 00
+- C => 01
+- G => 10
+- U => 11
 """
 struct ReferenceSequence
     data::Vector{UInt64}
@@ -65,7 +65,7 @@ function get_refseq_pos(refseq, pos)
 end
 
 """
-    encode_refseq(::String) :: Reference Sequence
+    encode_refseq(::String) :: ReferenceSequence
 
 Function that takes a string containing only A, C, G, and U and encodes it into a ReferenceSequence type
 """
@@ -221,7 +221,11 @@ function save_RefSeq(path::String=PATH)
     @save "$(path)Human_mRNA_allRefSeq.bson" allRefSeq
 end
 
+"""
+    reverse_complement(::String) :: String
 
+Takes the reverse complement of an RNA string
+"""
 function reverse_complement(pattern::String) :: String
     pattern = uppercase(pattern)
     BASES = ['A', 'C', 'G', 'U']
@@ -235,6 +239,37 @@ function reverse_complement(pattern::String) :: String
     return r_c
 end
 
+"""
+    calculate_Peq(::String) :: Array{UInt64, 1}
+
+Takes as input an RNA strand and translates it into an array of Unsigned Integers each one representing one letter and each bit referring to a position.  
+Used as a pre-processing step for Myers searching algorithm.  Maximum length of searched pattern is 64.
+
+### Example
+
+AACGCU becomes [110000, 001010, 000100, 000001]
+"""
+function calculate_Peq(pattern::String) :: Array{UInt64, 1}
+    m = length(pattern)
+    Peq :: Array{UInt64, 1} = zeros(UInt64, length(RNA_ALPHABET))
+    for i in 1:m
+        y = pattern[i]
+        for x in RNA_ALPHABET
+            if x == y
+                Peq[BASES[x] + 1] |= one(UInt64) << (i-1)
+            end
+        end
+    end
+    Peq
+end
+
+"""
+    motif_to_transcript_match(Peq::Array{Unit64, 1}, m::Int64, refseq::ReferenceSequence, min_K::Int) :: Array{Tuple{UInt64, UInt64}}
+
+Takes as input Peq calculated in [calculate_Peq](@ref), m = the length of the pattern to search for, the ReferenceSequence to search in, and min_k = 
+the maximum distance to output.  Implements the string matching algorithm described in _A Fast Bit-Vector Algorithm for Approximate String Matching Based
+on Dynamic Programming_ by Gene Myers.  Output is a tuple of all Levenshtein distances less than min_k and their positions in refseq.   
+"""
 function motif_to_transcript_match(Peq::Array{UInt64, 1}, m::Int64,  refseq::ReferenceSequence, min_k::Int) :: Array{Tuple{UInt64, UInt64}}
     min_j = 0
     out::Array{Tuple{UInt64, UInt64}} = []
@@ -267,7 +302,12 @@ function motif_to_transcript_match(Peq::Array{UInt64, 1}, m::Int64,  refseq::Ref
 end
 
 
+"""
+    find_match_sequence(motif::String, sequence::String, mismatches::Int) :: Array{String, 1}
 
+Function takes as input a motif, sequence, and number of mismatches to search for.  Output is an array of all substrings of sequence which have a 
+Hamming distance of exactly mismatches to motif. 
+"""
 function find_match_sequences(motif::String, sequence::String, mismatches::Int) :: Array{String, 1}
     mtchs::Array{String, 1} = []
     for i in 1:(length(sequence) - length(motif) + 1)
@@ -276,6 +316,11 @@ function find_match_sequences(motif::String, sequence::String, mismatches::Int) 
     return mtchs
 end
 
+"""
+    mismatch_positions(::String, ::String) :: Array{Int, 1}
+
+Function returns an array of all positions in which strings used as input differ.  Strings must be the same length.
+"""
 function mismatch_positions(seq1::String, seq2::String) :: Array{Int, 1}
     out::Array{Int, 1} = []
     for i in 1:length(seq1)
@@ -288,21 +333,14 @@ function mismatch_positions(seq1::String, seq2::String) :: Array{Int, 1}
     return out
 end
 
-function calculate_Peq(pattern::String) :: Array{UInt64, 1}
-    m = length(pattern)
-    Peq :: Array{UInt64, 1} = zeros(UInt64, length(RNA_ALPHABET))
-    for i in 1:m
-        y = pattern[i]
-        for x in RNA_ALPHABET
-            if x == y
-                Peq[BASES[x] + 1] |= one(UInt64) << (i-1)
-            end
-        end
-    end
-    Peq
-end
 
+"""
+    find_genome_matches(pattern::String, excluded_gene::String = "", verbose::Bool = true, minimum_matches = 5) :: Array{Tuple{String, Int64}}
 
+Function takes as input a pattern to search the genome for, excluded_gene to exclude the gene of interest from the search, verbose set to true displays
+a progress bar showing progress of search, and minimum_matches which is the amount of mismatches searched for + 1.  Output is an array of tuples of transcript names
+and the lowest Hamming distance found to the pattern within that transcript.
+"""
 function find_genome_matches(pattern::String, excluded_gene::String = "",  verbose::Bool = true, minimum_matches = 5) :: Array{Tuple{String, Int64}}
     (length(ALLREFSEQ) == 0) && return []
     out::Array{Tuple{String, Int64}} = []
@@ -327,6 +365,12 @@ function find_genome_matches(pattern::String, excluded_gene::String = "",  verbo
     out
 end
 
+"""
+    compress_genome_matches(::Array{Tuple{String, Int64}}) :: Dict{String, Array{Int64, 1}}
+
+Function takes as input the output of [find_genome_matches](@ref) and collapses it into a dictionary of Gene names and an array of all lowest Hamming mismatch 
+of the transcripts of that gene.
+"""
 function compress_genome_matches(raw_data::Array{Tuple{String, Int64}}) :: Dict{String, Array{Int64, 1}}
     out = Dict{String, Array{Int64, 1}}()
     (length(TRANSCRIPTGENE) == 0) && return out
@@ -340,6 +384,13 @@ function compress_genome_matches(raw_data::Array{Tuple{String, Int64}}) :: Dict{
     return out
 end
 
+
+"""
+    final_calc(pattern::String, raw_data::Array{Tuple{String, Int64}}, compressed_data::Dict{String, Array{Int64, 1}})
+
+Function takes as input the pattern being searched for, the raw_data from [find_genome_matches](@ref), and the compressed data from [compress_genome_matches](@ref).
+Ouput is a Tuple containing the mismatch_counts dictionary which adds up the number of genes with a minimum Hamming distance of 0-4, and the specificity score.
+"""
 function final_calc(pattern::String, raw_data::Array{Tuple{String, Int64}}, compressed_data::Dict{String,Array{Int64, 1}})
     mismatch_counts = Dict{Int64, Int64}([x => 0 for x in 0:4])
     gene_correction = Dict()
@@ -348,18 +399,11 @@ function final_calc(pattern::String, raw_data::Array{Tuple{String, Int64}}, comp
     min_match::Int64 = 5
     for (gene, matches) in compressed_data
         filter!(x -> x == minimum(matches), matches)
-        #(length(matches) > 1) && (transcript_list = vcat(transcript_list, GENETRANSCRIPTS[gene]))
         mismatch_counts[minimum(matches)] += 1
         (minimum(matches) < min_match) && (min_match = minimum(matches))
         gene_correction[gene] = []
     end
     for (name, match) in raw_data
-        #if minimum(match) == minimum(compressed_data[TRANSCRIPTGENE[name]]) && name in transcript_list
-        #    match_patterns = find_match_sequences(pattern, decode_refseq(ALLREFSEQ[name]), minimum(match))
-        #    for match_pattern in match_patterns
-        #        push!(gene_correction[TRANSCRIPTGENE[name]], mismatch_positions(pattern, match_pattern))
-        #    end
-        #end
         if minimum(match) == min_match
             match_patterns = find_match_sequences(pattern, decode_refseq(ALLREFSEQ[name]), min_match)
             for match_pattern in match_patterns
@@ -388,7 +432,13 @@ function final_calc(pattern::String, raw_data::Array{Tuple{String, Int64}}, comp
     (mismatch_counts, min_score)
 end
 
+"""
+    Calculate_Specificity(patterns, excluded_gene="", rg=2:18, verbose=true) :: DataFrame
 
+Function takes as input the pattern to be searched against the genome, the excluded gene to be ignored, the range of pattern being used, and a boolean which
+controls whether progress bars will be shown.  Output is a DataFrame with a column for the pattern, the number of genes with minimum mismatch distance of 0-4
+and the specificity score.
+"""
 function Calculate_Specificity(patterns, excluded_gene::String="", rg::UnitRange{Int64} = 2:18, verbose::Bool=true) :: DataFrame
     string_array = Array{String, 1}()
     for pattern in patterns
